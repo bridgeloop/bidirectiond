e84322308fa6b49c6a78d0d8951c362c16a779ba
aiden <aiden@citalopram.reviews>
2022-03-07 21:13:54 +0000

    it was wasting a byte before
    i promise this program aint completely littered with logic bugs

--- src/cp_pwd.c
+++ src/cp_pwd.c
@@ -11,7 +11,7 @@ int bdd_cp_pwd(char *dest, int dest_sz, int rwflag, void *_ctx) {
 		return 0;
 	}
 	int src_len = (int)strlen(src);
-	if (dest_sz <= 0 || src_len <= 0 || src_len >= dest_sz) {
+	if (dest_sz <= 0 || src_len <= 0 || src_len > dest_sz) {
 		return 0;
 	}
 	memcpy(dest, src, src_len);
