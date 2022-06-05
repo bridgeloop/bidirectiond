9b4d1a7bec617991fa6529ca89dd5b3953dcbb5b
aiden <aiden@citalopram.reviews>
2022-06-05 11:38:48 +0100

    bdd_shutdown_status.h

---
+++ core/src/headers/bdd_shutdown_status.h
@@ -0,0 +1,10 @@
+#ifndef bidirectiond_core__bdd_shutdown_status__h
+#define bidirectiond_core__bdd_shutdown_status__h
+
+enum bdd_shutdown_status {
+	bdd_shutdown_discard,
+	bdd_shutdown_inprogress,
+	bdd_shutdown_complete,
+};
+
+#endif
