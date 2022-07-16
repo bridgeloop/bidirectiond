d327629ce1befcff62ebdda1d65736be801749c0
aiden <aiden@citalopram.reviews>
2022-07-16 17:37:39 +0100

    bdd_io_id is now always an unsigned short

--- core/src/headers/bidirectiond_n_io.h
+++ core/src/headers/bidirectiond_n_io.h
@@ -1,6 +1,9 @@
+#ifndef bdd__bidirectiond_n_io__h
+#define bdd__bidirectiond_n_io__h
+typedef unsigned short int bdd_io_id;
 #ifndef BIDIRECTIOND_N_IO
 #define BIDIRECTIOND_N_IO 2
 #endif
-#ifndef bdd_io_id
-#define bdd_io_id typeof(BIDIRECTIOND_N_IO)
+#include <assert.h>
+static_assert(BIDIRECTIOND_N_IO < USHRT_MAX, "BIDIRECTIOND_N_IO is greater than or equal to USHRT_MAX");
 #endif
